9ec494aaadb2ef5dc16d000037846419680be6cf
Niall Devlin <niallcdevlin@gmail.com>
2023-03-03 21:51:10 -0700

    Updated gitignore

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
 import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense
